eeddf39ad02e56d7e388ed933745d0b7442c8c9b
BatMan-yadav <akashyadav121mrt@gmail.com>
2024-03-02 12:46:37 +0530

    string

---
+++ basic js/06_string.js
@@ -0,0 +1,11 @@
+//string concatenation
+//1 method
+const name = "akash"
+let gmail = 121
+let gmail2 = "@gmail.com"
+console.log(name + gmail + gmail2)
+
+
+//2nd method recommended
+console.log(`gmail of user is ${name}${gmail}${gmail2}`)
+

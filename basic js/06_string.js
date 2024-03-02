d97eced5cf060a5aaa98e7de2fc0e07849c013ff
BatMan-yadav <akashyadav121mrt@gmail.com>
2024-03-02 13:02:20 +0530

    string

--- basic js/06_string.js
+++ basic js/06_string.js
@@ -9,3 +9,10 @@ console.log(name + gmail + gmail2)
 //2nd method recommended
 console.log(`gmail of user is ${name}${gmail}${gmail2}`)
 
+// stings method
+let value = "akash yadav"
+console.log(value.charAt(2))        //find chararter at index 2
+console.log(value.indexOf('a'))     //find the index of first "a"
+console.log(value.toUpperCase())    //convert string into uppercase
+console.log(value.toLowerCase())    //convert string into lowercase
+

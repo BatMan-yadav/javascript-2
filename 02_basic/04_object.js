dafabfdcb1cf788ca9e5f678120fc21d3037da6f
BatMan-yadav <akashyadav121mrt@gmail.com>
2024-03-06 11:33:37 +0530

    object learning2

--- 02_basic/04_object.js
+++ 02_basic/04_object.js
@@ -1,5 +1,13 @@
 // object {singleton}
 
+const user1 = new Object()
+
+// const user1 = {}
+user1.name = "yadav"
+user1.age = 15
+user1.isLoggedIn = false
+
+console.log(user1);
 
 
-
+//object in object (nesting object)

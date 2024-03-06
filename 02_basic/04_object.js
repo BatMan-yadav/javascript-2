1ff394076a7ba4386ac8846d5cfa18612a568154
BatMan-yadav <akashyadav121mrt@gmail.com>
2024-03-06 12:01:13 +0530

    object2

--- 02_basic/04_object.js
+++ 02_basic/04_object.js
@@ -11,3 +11,22 @@ console.log(user1);
 
 
 //object in object (nesting object)
+
+
+const userInfo = {
+    name : {
+        fullName : {
+            userid : "akash yadav"
+        }
+    }
+}
+
+console.log(userInfo.name.fullName.userid);
+
+// merging two object
+
+const obj1 = {1 : "a" , 2 : "b"}
+const obj2 = {2 : "c" , 3 : "d"}
+
+const obj3 = Object.assign({}, obj1 ,obj2)
+console.log(obj3);

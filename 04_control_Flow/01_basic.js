ffd863decf52c2856cfa7bf7bc3fdc8912334de7
BatMan-yadav <akashyadav121mrt@gmail.com>
2024-03-08 17:59:22 +0530

    conditional

---
+++ 04_control_Flow/01_basic.js
@@ -0,0 +1,29 @@
+//if statement
+
+const userLoggedIn = true;
+if(userLoggedIn == true){
+    console.log("user loggin sucessfully .....");
+}
+
+const temp = 45
+if (temp <= 50) {
+    console.log("temperature is less tham 50 deg cel.");    
+}
+
+// else
+
+const age = 67
+// const age = 12
+
+if (age <= 18) {
+    console.log("you cannot vote...");
+}
+else{
+    console.log("you can vote...");
+}
+const loggin = true
+const debitCard = true
+
+console.log("yon are ready to shoping....");
+if(loggin == true && debitCard == true){
+}
